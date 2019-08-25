ab8103fc5dde31fea1e29871e2cb00ae062efc05
stevenallen05 <sallen@amberstyle.ca>
2019-08-24 23:28:48 -0700

    Add `*_delegated?` method (#2)
    * Guardconfig
    
    * Adding test
    
    * Implement module changes

--- lib/delegate_if_nil.rb
+++ lib/delegate_if_nil.rb
@@ -20,6 +20,10 @@ module DelegateIfNil
         else
           return self.send(to)&.send(attr).nil? ? "unset" : to.to_s
         end
+
+        delegted_bool_method = "#{attr}_delegated?".to_sym do
+          self[attr].nil?
+        end
         # return self.send(to).send(source_method) if self.send(to)&.respond_to?(source_method)
         # return to.to_s unless self.send(to).send(attr).nil?
         # "unset"
